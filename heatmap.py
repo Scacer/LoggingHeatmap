6ae9be1b5fa8a1b1af4165947020f814d02172b5
Scacer <60669273+Scacer@users.noreply.github.com>
2024-05-31 16:32:55 +0100

    Program Skeleton
    Basic skeleton program created for use during testing.

---
+++ heatmap.py
@@ -0,0 +1,13 @@
+class Heatmap:
+
+    def __init__(self, heatmapInfo):
+        self.heatmapData = []
+
+    def loadHeatmapData():
+        print("Load heatmap data here")
+
+    def constructHeatmap():
+        print("Construct the Heatmap here")
+
+    def inputHeatmapData():
+        print("Add Heatmap data here")
