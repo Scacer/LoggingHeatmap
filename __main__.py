48d1ee5622e14363f942e2a95bed2001b2e82394
Scacer <60669273+Scacer@users.noreply.github.com>
2024-06-03 09:49:34 +0100

    Heatmap Construction Implemented
    Basic framework for heatmap construction implemented.

--- __main__.py
+++ __main__.py
@@ -3,7 +3,9 @@ import heatmap
 def main():
     testHeatmap1 = heatmap.Heatmap()
     print(testHeatmap1.getHeatmapData())
+    testHeatmap1.inputHeatmapData(2, 5)
     testHeatmap1.saveHeatmapData()
+    testHeatmap1.constructHeatmap()
     
 
 if __name__ == '__main__':
--- heatmap.py
+++ heatmap.py
@@ -1,7 +1,15 @@
+import matplotlib.image as image
+import matplotlib.pyplot as plt
+import numpy as np
+
 import matplotlib
+import matplotlib as mpls
 
 class Heatmap:
+    imgPath = ("D:\\Code\\Python\\LoggingHeatmap\\LoggingHeatmap\\map.png")
     heatmapData = []
+    rowsLabels = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    columnsLabels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
     
     # Constructor
     def __init__(self, filepath=None):
@@ -18,8 +26,8 @@ class Heatmap:
         
 
     # Service Methods
-    def inputHeatmapData():
-        print("Add Heatmap data here")
+    def inputHeatmapData(self, x, y, sightings=1):
+        self.heatmapData[y][x] += sightings
 
     def saveHeatmapData(self):
         with open('data.txt', 'w') as file:
@@ -36,7 +44,26 @@ class Heatmap:
                 if columncount != 10:
                     file.write("\n")
 
+    def constructHeatmap(self):
+        graphData = np.array(self.heatmapData)
 
+        fig, ax = plt.subplots()
+        im = ax.imshow(graphData)
+
+        ax.set_xticks(np.arange(len(self.rowsLabels)), labels=self.rowsLabels)
+        ax.set_yticks(np.arange(len(self.columnsLabels)), labels=self.columnsLabels)
+
+        plt.setp(ax.get_xticklabels(), rotation=45, ha="right", rotation_mode="anchor")
+
+        for i in range(len(self.columnsLabels)):
+            for j in range(len(self.rowsLabels)):
+                text = ax.text(j, i, graphData[i, j], ha="center", va="center", color="w")
+
+
+        ax.set_title("Illegal Logging Activity")
+        fig.tight_layout()
+        plt.show()
+        
 
     # Support Methods
     
