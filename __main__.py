5342dbf11902c8248869d663acb34a15b712ca1f
DenisKomarovs <deniskomarovs@outlook.com>
2024-06-10 13:26:05 +0100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,12 +1,24 @@
-#added all the libraries you need to install currently 
-#pip install matplotlib numpy pillow
-#pip install tk
-
 from heatmap_app import HeatmapApp
+import heatmap
+import string
+import random
 
 def main():
     app = HeatmapApp()
     app.mainloop()
+
+    testHeatmap1 = heatmap.Heatmap()
+    for i in range(0, 100):
+        testString = random.choice(string.ascii_letters[0:10]) + str(random.randint(1, 10))
+        testHeatmap1.inputHeatmapData(testString, "test")
+    testHeatmap1.inputHeatmapData("J2", "")
+    testHeatmap1.saveHeatmapData()
+    testHeatmap1.constructHeatmap()
+
+    testHeatmap2 = heatmap.Heatmap(filePath="./data.txt")
+    testHeatmap2.constructHeatmap()
+
+    testHeatmap3 = heatmap.Heatmap(isGuide=True)
 
 if __name__ == '__main__':
     main()
